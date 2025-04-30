49826a71f882b242c361b3b03e2595648ec56d2a
Amarildo <amarildop1dev@gmail.com>
2025-04-30 01:02:57 -0300

    Prática  - Calc Peso Corporal

--- pratica1/index.php
+++ pratica1/index.php
@@ -1,8 +1,25 @@
 <?php
 
+    $peso = 70;
+    $altura = 1.70;
 
+    $IMC =  $peso / ($altura * $altura) ;
 
+    echo 'IMC: '. $IMC. '</br>';
 
+    if($IMC >= 40 ){
+        echo 'Obesidade Grau 3';
+    }elseif($IMC >= 35){
+        echo 'Obesidade Grau 2';
+    }elseif($IMC >= 30){
+        echo 'Obesidade Grau 1';
+    }elseif($IMC >= 25){
+        echo 'Excesso de Peso';
+    }elseif($IMC >= 18.5){
+        echo 'Normal';
+    }else{
+        echo 'Baixo Peso';
+    }
 
 
 ?>

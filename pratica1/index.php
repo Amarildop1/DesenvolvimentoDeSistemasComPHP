8a5c6b689672fb41a2533cdad898db8f533fd47f
Amarildo <amarildop1dev@gmail.com>
2025-05-01 02:28:20 -0300

    index.php | Cálculo do IMC | Peso Corporal

--- pratica1/index.php
+++ pratica1/index.php
@@ -1,26 +1,56 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PESO CORPORAL - imc</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="form-container">
+            <h1>Calculadora de IMC</h1>
 
-    $peso = 70;
-    $altura = 1.70;
+            <form action="" method="GET">
+                <label for="peso">Peso (kg):</label>
+                <input type="number" name="peso" id="peso" required step="0.1" min="1" placeholder="Ex: 60">
 
-    $IMC =  $peso / ($altura * $altura) ;
+                <label for="altura">Altura (em metros): </label>
+                <input type="number" name="altura" id="altura" required step="0.01" min="1" placeholder="Ex: 1.65">
 
-    echo 'IMC: '. $IMC. '</br>';
+                <button type="submit"> Calcular </button>
+            </form>
 
-    if($IMC >= 40 ){
-        echo 'Obesidade Grau 3';
-    }elseif($IMC >= 35){
-        echo 'Obesidade Grau 2';
-    }elseif($IMC >= 30){
-        echo 'Obesidade Grau 1';
-    }elseif($IMC >= 25){
-        echo 'Excesso de Peso';
-    }elseif($IMC >= 18.5){
-        echo 'Normal';
-    }else{
-        echo 'Baixo Peso';
-    }
+            <!-- Aqui E sem fechar -->
+            <?php
 
+                /* $peso = 70;
+                $altura = 1.70; */
+                $peso = $_GET['peso'];
+                $altura = $_GET['altura'];
+                
+                $imc =  $peso / ($altura * $altura) ;
 
-?>
+                echo "<div class='resultado'>Seu IMC é: " . $imc . "</div>";
+
+                echo "<div class='conceito'>";
+                    if($imc >= 40 ){
+                        echo 'Obesidade Grau 3';
+                    }elseif($imc >= 35){
+                        echo 'Obesidade Grau 2';
+                    }elseif($imc >= 30){
+                        echo 'Obesidade Grau 1';
+                    }elseif($imc >= 25){
+                        echo 'Excesso de Peso';
+                    }elseif($imc >= 18.5){
+                        echo 'Normal';
+                    }else{
+                        echo 'Baixo Peso';
+                    }
+                echo "</div>";
+
+            ?>
+        </div> <!-- Final .form-container -->
+
+    </body>
+</html>
 

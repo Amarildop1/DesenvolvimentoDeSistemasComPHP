f4de977d3a3b800b4d570c8fffb387c86a71e0b2
Amarildo <amarildop1dev@gmail.com>
2025-05-16 22:22:57 -0300

    Adicionando meses em Português | Adicionado feriados | Destacando os feriados através do CSS

--- pratica2/calendario.php
+++ pratica2/calendario.php
@@ -1,17 +1,36 @@
 <?php
 
     function gerarCalendario($ano) {
+        $meses = [
+            1 => 'Janeiro', 2 => 'Fevereiro', 3 => 'Março',
+            4 => 'Abril', 5 => 'Maio', 6 => 'Junho',
+            7 => 'Julho', 8 => 'Agosto', 9 => 'Setembro',
+            10 => 'Outubro', 11 => 'Novembro', 12 => 'Dezembro'
+        ];
+
+        $feriadosFixos = [
+            "01-01" => "Ano Novo",
+            "04-21" => "Tiradentes",
+            "05-01" => "Dia do Trabalho",
+            "09-07" => "Independência",
+            "10-12" => "Nossa Senhora Aparecida",
+            "11-02" => "Finados",
+            "11-15" => "Proclamação da República",
+            "11-20" => "Dia Nacional de Zumbi",
+            "12-25" => "Natal"
+        ];
 
         echo '<div class="calendario">';
 
             // Iterar por cada mês do ano
             for ($mes = 1; $mes <= 12; $mes++) {
                 echo '<div class="mes">';
-                    echo '<h2>' . date('F', mktime(0, 0, 0, $mes, 10)) . ' ' . $ano . '</h2>';
+
+                    echo '<h2>' . $meses[$mes] . ' ' . $ano . '</h2>';
 
                     echo '<table>';
                         echo '<tr>';
-                            /* Dias da semana */
+
                             $diaDaSemana = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'];
                             foreach ($diaDaSemana as $dia) {
                                 echo '<th>' . $dia . '</th>';
@@ -30,10 +49,21 @@
 
                             // Preenchendo os dias do mês
                             for ($dia = 1; $dia <= $diasNoMes; $dia++) {
+
+                                $dataAtual = sprintf("%02d-%02d", $mes, $dia);
+                                $classe = '';
+                                $titulo = '';
+
+                                if (isset($feriadosFixos[$dataAtual])) {
+                                    $classe = 'feriado';
+                                    $titulo = 'title="' . $feriadosFixos[$dataAtual] . '"';
+                                }
+
                                 if (($i % 7) == 0 && $i != 0) {
                                     echo '</tr><tr>';
                                 }
-                                echo '<td>' . $dia . '</td>';
+
+                                echo "<td class='$classe' $titulo>$dia</td>";
                                 $i++;
                             }
 
